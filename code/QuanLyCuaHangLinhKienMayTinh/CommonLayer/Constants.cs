0348a431a126b738ecf70bbebd08fd1e22849dd6
cuongeuro226 <cuongeuro226@users.noreply.github.com>
2015-12-12 03:40:38 +0700

    Update Library CommonLayer

--- code/QuanLyCuaHangLinhKienMayTinh/CommonLayer/Constants.cs
+++ code/QuanLyCuaHangLinhKienMayTinh/CommonLayer/Constants.cs
@@ -9,11 +9,17 @@ namespace CommonLayer
 {
     public class Constants
     {
-        public static string ConnectionStrnig =
+        public static string ConnectionString =
             ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
 
         public static string MsgExceptionSql = "Lỗi kết nối";
 
         public static string MsgExceptionError = "Lỗi hệ thống";
+
+        public static string MsgNotificationSuccessfuly = "Thêm thành công!";
+
+        public static string MsgNotificationEditSuccessfuly = "Sửa thành công!";
+
+        public static string MsgAlreadyExist = "Dữ liệu đã tồn tại hoặc lỗi dữ liệu đầu vào.";
     }
 }
