e4461218fdac50c7d964205019063bb93431324d
cuongeuro226 <cuongeuro226@users.noreply.github.com>
2015-12-11 22:13:30 +0700

    Commonlayer

---
+++ code/QuanLyCuaHangLinhKienMayTinh/CommonLayer/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLayer
+{
+    public class Constants
+    {
+        public static string ConnectionStrnig =
+            ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+        public static string MsgExceptionSql = "Lỗi kết nối";
+
+        public static string MsgExceptionError = "Lỗi hệ thống";
+    }
+}
