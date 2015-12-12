49c6a7d27e8531b64076800bcb8a2d22e28efcf4
cuongeuro226 <cuongeuro226@users.noreply.github.com>
2015-12-12 13:22:03 +0700

    Bll product

---
+++ code/QuanLyCuaHangLinhKienMayTinh/BLL/Warehouse/BllProduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Warehouse;
+
+namespace BLL.Warehouse
+{
+    public class BllProduct
+    {
+        private DalProduct _dalProduct;
+
+        public BllProduct()
+        {
+            _dalProduct = new DalProduct();
+        }
+        public DataTable GetListProducts()
+        {
+            return _dalProduct.GetListProducts();
+        }
+    }
+}
